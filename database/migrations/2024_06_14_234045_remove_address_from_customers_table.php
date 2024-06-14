e002fb782175dff1e18919561978133b70435d05
winex01 <winnie131212592@gmail.com>
2024-06-15 07:41:58 +0800

    fix migration table name

--- database/migrations/2024_06_14_234045_remove_address_from_customers_table.php
+++ database/migrations/2024_06_14_234045_remove_address_from_customers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('customer', function (Blueprint $table) {
+        Schema::table('customers', function (Blueprint $table) {
             //
             $table->dropColumn('address');
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('customer', function (Blueprint $table) {
+        Schema::table('customers', function (Blueprint $table) {
             //
             $table->string('address')->nullable();
         });
