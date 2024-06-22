c9a58f814d6cf341af9ce50cead2958c168b5985
winex01 <winnie131212592@gmail.com>
2024-06-22 13:04:12 +0800

    fix customer migration and remove web tinker

--- database/migrations/2024_06_11_071751_create_customers_table.php
+++ database/migrations/2024_06_11_071751_create_customers_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
             $table->string('last_name');
+            $table->string('first_name');
             $table->date('date_of_birth')->nullable();
             $table->string('contact_number')->nullable();
             $table->string('email')->nullable();
