4897246e653d97a709d8d92caa49dbde722efacc
winex01 <winnie131212592@gmail.com>
2024-06-23 18:45:22 +0800

    fix crud

--- app/Http/Controllers/Admin/CustomerCrudController.php
+++ app/Http/Controllers/Admin/CustomerCrudController.php
@@ -102,9 +102,6 @@ class CustomerCrudController extends CrudController
             'view_namespace' => 'signature-field-for-backpack::fields',
         ]);
 
-        // $this->crud->modifyField('notes', ['type' => 'textarea']);
-        $this->crud->modifyField('date_of_birth', ['type' => 'date']);        
-    
     }
 
     /**
