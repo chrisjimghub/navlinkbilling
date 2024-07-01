5debd7b4ff9691bf4414a6fa7a052c1d48560244
winex01 <winnie131212592@gmail.com>
2024-07-01 16:12:56 +0800

    fix lang

--- lang/en/app.php
+++ lang/en/app.php
@@ -34,8 +34,8 @@ return [
     'billing_particulars' => 'Particulars',
     'billing_description' => 'Description',
     'billing_period' => 'Billing Period',
-    'billing_unique_account_billing_type_installation' => 'The selected account already has a billing type Installation Fee.',
-    'billing_unique_account_billing_type_monthly' => 'The selected account already has unpaid billing type Monthly Fee.',
+    'billing_unique_account_billing_type_installation' => 'The selected account already has a billing Installation Fee.',
+    'billing_unique_account_billing_type_monthly' => 'The selected account already has unpaid billing Monthly Fee.',
 
     // Billing Type
     'billing_type' => 'Billing Type',
