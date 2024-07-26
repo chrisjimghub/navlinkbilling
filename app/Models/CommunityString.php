ae0f8e8886f4f35dea63b6f20ab2a548a104a97c
winex01 <winnie131212592@gmail.com>
2024-07-26 19:21:37 +0800

    use base model on cummonity string model

--- app/Models/CommunityString.php
+++ app/Models/CommunityString.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
+
 
 class CommunityString extends Model
 {
-    use CrudTrait;
-    use HasFactory;
 
     /*
     |--------------------------------------------------------------------------
