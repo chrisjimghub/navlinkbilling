76441363895530454221a9bbd85aa963a1c137d8
winex01 <winnie131212592@gmail.com>
2024-08-05 11:40:05 +0800

    add jumbotron welcome

--- resources/views/vendor/backpack/ui/customer/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/customer/dashboard.blade.php
@@ -1,21 +1,21 @@
 @extends(backpack_view('blank'))
 @php
-    // if (backpack_theme_config('show_getting_started')) {
-    //     $widgets['before_content'][] = [
-    //         'type'        => 'view',
-    //         'view'        => backpack_view('inc.getting_started'),
-    //     ];
-    // } else {
-    //     $widgets['before_content'][] = [
-    //         'type'        => 'jumbotron',
-    //         'heading'     => trans('backpack::base.welcome'),
-    //         'heading_class' => 'display-3 '.(backpack_theme_config('layout') === 'horizontal_overlap' ? ' text-white' : ''),
-    //         'content'     => trans('backpack::base.use_sidebar'),
-    //         'content_class' => backpack_theme_config('layout') === 'horizontal_overlap' ? 'text-white' : '',
-    //         'button_link' => backpack_url('logout'),
-    //         'button_text' => trans('backpack::base.logout'),
-    //     ];
-    // }
+    if (backpack_theme_config('show_getting_started')) {
+        $widgets['before_content'][] = [
+            'type'        => 'view',
+            'view'        => backpack_view('inc.getting_started'),
+        ];
+    } else {
+        $widgets['before_content'][] = [
+            'type'        => 'jumbotron',
+            'heading'     => 'Welcome, '.auth()->user()->customer->last_name.' '.auth()->user()->customer->first_name.'!',
+            'content'     => __('Experience seamless payments with Gcash E-Wallet – faster, easier, and more convenient for all your billing needs!'),
+            'heading_class' => 'display-6 '.(backpack_theme_config('layout') === 'horizontal_overlap' ? ' text-white' : ''),
+            'content_class' => backpack_theme_config('layout') === 'horizontal_overlap' ? 'text-white' : '',
+            'button_link' => backpack_url('logout'),
+            'button_text' => trans('backpack::base.logout'),
+        ];
+    }
 @endphp
 
 
