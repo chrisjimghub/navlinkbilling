2d801093c01dc33ccca71abaae3b0a0a61998e35
winex01 <winnie131212592@gmail.com>
2024-08-06 10:22:22 +0800

    register config make it default false

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -14,7 +14,7 @@ return [
     | By default the registration is open only on localhost.
     */
 
-    'registration_open' => env('BACKPACK_REGISTRATION_OPEN', env('APP_ENV') === 'local'),
+    'registration_open' => env('BACKPACK_REGISTRATION_OPEN', false),
 
     /*
     |--------------------------------------------------------------------------
