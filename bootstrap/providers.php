4f902521a98e1fbe059cdee11cffa9f22bd13a0b
winex01 <winnie131212592@gmail.com>
2024-08-06 11:48:21 +0800

    remove setting providers class

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\BackpackSettingsServiceProvider::class,
     App\Providers\CustomizeBackpackControllerServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
