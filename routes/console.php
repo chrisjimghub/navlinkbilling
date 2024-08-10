02fd3a341b2aaaf7048e2a75774aeb85c3ab6258
winex01 <winnie131212592@gmail.com>
2024-08-10 22:32:48 +0800

    add auto bill send nofitication route console

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('bill:auto-generate')->everyMinute()->withoutOverlapping();
-// Schedule::command('bill:auto-send-notification')->everyMinute()->withoutOverlapping();
+Schedule::command('bill:auto-send-notification')->everyMinute()->withoutOverlapping();
 // Schedule::command('bill:auto-send-cut-off-notification')->everyMinute()->withoutOverlapping();
