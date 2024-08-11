d19053d12e482aed2d6728fd474d490d00ca70e4
winex01 <winnie131212592@gmail.com>
2024-08-11 12:26:25 +0800

    fix auth rotues for customers namespace

--- routes/customer.php
+++ routes/customer.php
@@ -8,9 +8,22 @@ Route::group([
         (array) config('backpack.base.web_middleware', 'web'),
         (array) config('backpack.base.middleware_key', 'admin'),
     ),
-    'namespace' => 'App\Http\Controllers\Customer',
 ], function () { 
-    Route::crud('theme', 'ThemeCrudController');
-    Route::crud('dashboard', 'DashboardCrudController');
-    Route::crud('billing-history', 'BillingHistoryCrudController');
-}); 
+    // Group with 'App\Http\Controllers\Customer' namespace
+    Route::group(['namespace' => 'App\Http\Controllers\Customer'], function() {
+        Route::crud('theme', 'ThemeCrudController');
+        Route::crud('dashboard', 'DashboardCrudController');
+        Route::crud('billing-history', 'BillingHistoryCrudController');
+    });
+
+    // Group with 'Backpack\CRUD\app\Http\Controllers' namespace
+    Route::group(['namespace' => 'Backpack\CRUD\app\Http\Controllers'], function() {
+        Route::get('edit-account-info', 'MyAccountController@getAccountInfoForm')->name('backpack.account.info');
+        Route::post('edit-account-info', 'MyAccountController@postAccountInfoForm')->name('backpack.account.info.store');
+        Route::post('change-password', 'MyAccountController@postChangePasswordForm')->name('backpack.account.password');
+
+        // logout
+        Route::get('logout', 'Auth\LoginController@logout')->name('backpack.auth.logout');
+        Route::post('logout', 'Auth\LoginController@logout');
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+    'namespace' => 'Backpack\CRUD\app\Http\Controllers',
+    'middleware' => config('backpack.base.web_middleware', 'web'),
+],
+function () {
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('backpack.auth.login');
+    Route::post('login', 'Auth\LoginController@login');
+
+    // forgot password
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('backpack.auth.password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('backpack.auth.password.reset.token');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('backpack.auth.password.email')->middleware('backpack.throttle.password.recovery:'.config('backpack.base.password_recovery_throttle_access'));
+
+    // verify email
+    Route::get('email/verify', 'Auth\VerifyEmailController@emailVerificationRequired')->name('verification.notice');
+    Route::get('email/verify/{id}/{hash}', 'Auth\VerifyEmailController@verifyEmail')->name('verification.verify');
+    Route::post('email/verification-notification', 'Auth\VerifyEmailController@resendVerificationEmail')->name('verification.send');
+});
+
+
+Route::get('/dashboard', function () {
+    return redirect('customer/dashboard');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Route::get('/customer', function () {
     return redirect('customer/dashboard');
 });
 
-Route::get('/customer/logout', function () {
-    return redirect(config('backpack.base.route_prefix') . '/logout');
-});
 
 require __DIR__.'/customer.php';
