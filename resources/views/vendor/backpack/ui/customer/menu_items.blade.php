450f59eb90b558315ba7942cb84c099afe72e8b1
winex01 <winnie131212592@gmail.com>
2024-08-21 13:54:32 +0800

    show only piso wifi crud if has piso wifi account

--- resources/views/vendor/backpack/ui/customer/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/customer/menu_items.blade.php
@@ -5,5 +5,16 @@
 <x-backpack::menu-dropdown title="Accounts" icon="las la-folder-open">
     {{-- <x-backpack::menu-dropdown-header title="Authentication" /> --}}
     <x-backpack::menu-dropdown-item title="Invoice History" icon="las la-file-invoice" :link="backpack_url('billing-history')" />
-    <x-backpack::menu-dropdown-item title="Piso Wifi" icon="las la-credit-card" :link="backpack_url('test')" />
+
+    @php
+        $customerId = auth()->user()->customer_id;
+        $hasPisoWifi = modelInstance('Account')->harvestCrud()->where('customer_id', $customerId)->exists();
+    @endphp
+
+    @if($hasPisoWifi)
+        <x-backpack::menu-dropdown-item title="Piso Wifi" icon="las la-credit-card" :link="backpack_url('test')" />
+    @endif
+
 </x-backpack::menu-dropdown>
+
+
