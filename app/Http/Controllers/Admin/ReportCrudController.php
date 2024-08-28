5c008414e41f69c9b2addcef4d852869ecca46c9
winex01 <winnie131212592@gmail.com>
2024-08-28 18:37:40 +0800

    remove unused request

--- app/Http/Controllers/Admin/ReportCrudController.php
+++ app/Http/Controllers/Admin/ReportCrudController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Admin\Traits\CrudExtend;
 use App\Http\Controllers\Admin\Traits\FetchOptions;
-use App\Http\Requests\ReportRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 use Winex01\BackpackFilter\Http\Controllers\Operations\ExportOperation;
